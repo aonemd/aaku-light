26a384cb05512c4ceb17f2a52495e5ac02c2a445
aonemd <aonemdsaleh@gmail.com>
2018-01-03 14:26:14 +0200

    Add Zsh config

---
+++ zsh/zsh/config.zsh
@@ -0,0 +1,49 @@
+# General
+autoload -U promptinit && promptinit
+autoload -U colors && colors
+
+setopt prompt_subst
+setopt correct
+setopt extendedglob
+unsetopt nomatch
+unsetopt beep
+setopt autocd
+
+# Autocompletion
+autoload -U compinit && compinit
+zmodload zsh/complist
+zstyle ':completion:*' menu select
+zstyle ':completion:*' list-colors "${(@s.:.)LS_COLORS}"
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+# use the vi navigation keys in menu completion
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
+
+# History
+export HISTSIZE=25000
+export SAVEHIST=$HISTSIZE
+export HISTFILE="$HOME/.zsh_history"
+export HISTORY_IGNORE="(ls|cd|pwd|exit|cd ..|sic|sic *)"
+# Ignore duplicate commands
+setopt HIST_IGNORE_ALL_DUPS
+# Ignore commands that start with a space
+setopt HIST_IGNORE_SPACE
+# add commands as they are typed, don't wait until shell exit
+setopt INC_APPEND_HISTORY
+# share hist between sessions
+setopt SHARE_HISTORY
+# Complete Search history
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search
+bindkey "^[[B" down-line-or-beginning-search
+
+# Vim Mode
+bindkey -v
+export KEYTIMEOUT=1
+bindkey '^?' backward-delete-char # backspace working afte command mode
+bindkey '^r' history-incremental-search-backward # ctrl-r starts searching history backward
--- zsh/zsh/prompt.zsh
+++ zsh/zsh/prompt.zsh
@@ -1,13 +1,43 @@
-function git_status {
-  if [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit, working tree clean" ]]; then
+PROMPT_INSERT_SYMBOL="›"
+PROMPT_NORMAL_SYMBOL="·"
+
+function prompt_redraw {
+    zle reset-prompt
+}
+zle -N zle-keymap-select prompt_redraw
+
+function prompt_current_dir {
+  echo -n "%{$fg[blue]%}%1~"
+}
+
+function prompt_symbol {
+  local current_symbol="$PROMPT_INSERT_SYMBOL"
+  [ "$KEYMAP" = 'vicmd' ] && current_symbol="$PROMPT_NORMAL_SYMBOL"
+  echo -n "%{$fg[white]%}$current_symbol"
+}
+
+function prompt_git_branch {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/ $(_prompt_git_status)\1/"
+}
+
+function _prompt_git_status {
+  if [ -n "$(git status --porcelain 2> /dev/null)" ]; then
     echo "%{$fg[red]%}"
   else
     echo "%{$fg[green]%}"
   fi
 }
 
-function git_branch {
-  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/ $(git_status)\1/"
+function prompt_hg_branch {
+  hg branch 2> /dev/null | sed -e "s/\(.*\)/ $(_prompt_hg_status)\1/"
 }
 
-PROMPT='%{$fg[blue]%}%1~$(git_branch) %{$fg_bold[white]%}➜ %{$reset_color%}'
+function _prompt_hg_status {
+  if [[ $( hg status 2> /dev/null ) != "" ]]; then
+    echo "%{$fg[red]%}"
+  else
+    echo "%{$fg[green]%}"
+  fi
+}
+
+PROMPT='$(prompt_current_dir)$(prompt_git_branch)$(prompt_hg_branch) $(prompt_symbol) %{$reset_color%}'
