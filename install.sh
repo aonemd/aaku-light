42f2c5b6411a717d367af06fcb7112daeeee60aa
aonemd <aonemdsaleh@gmail.com>
2019-04-20 14:20:06 +0200

    Install X11 tools only if a X server is running

--- install.sh
+++ install.sh
@@ -19,31 +19,33 @@ ln -sfn ${BASEDIR}/tmux/tmux.conf $HOME/.tmux.conf
 ln -sfn ${BASEDIR}/git/gitconfig $HOME/.gitconfig
 ln -sfn ${BASEDIR}/ag/agignore $HOME/.agignore
 
-[ -d $HOME/.config/cmus/ ] || mkdir -p $HOME/.config/cmus
-ln -sfn ${BASEDIR}/cmus/aaku.theme $HOME/.config/cmus/aaku.theme
+if [[ $DISPLAY ]]; then
+  [ -d $HOME/.config/cmus/ ] || mkdir -p $HOME/.config/cmus
+  ln -sfn ${BASEDIR}/cmus/aaku.theme $HOME/.config/cmus/aaku.theme
 
-ln -sfn ${BASEDIR}/X11/xinitrc $HOME/.xinitrc
-ln -sfn ${BASEDIR}/X11/Xresources $HOME/.Xresources
+  ln -sfn ${BASEDIR}/X11/xinitrc $HOME/.xinitrc
+  ln -sfn ${BASEDIR}/X11/Xresources $HOME/.Xresources
 
-ln -sfn ${BASEDIR}/i3 $HOME/.config/i3
+  ln -sfn ${BASEDIR}/i3 $HOME/.config/i3
 
-ln -sfn ${BASEDIR}/i3blocks $HOME/.config/i3blocks
+  ln -sfn ${BASEDIR}/i3blocks $HOME/.config/i3blocks
 
-ln -sfn ${BASEDIR}/bin/fehbg $HOME/.fehbg
+  ln -sfn ${BASEDIR}/bin/fehbg $HOME/.fehbg
 
-[ -d $HOME/.config/termite/ ] || mkdir -p $HOME/.config/termite
-ln -sfn ${BASEDIR}/termite/config $HOME/.config/termite/config
+  [ -d $HOME/.config/termite/ ] || mkdir -p $HOME/.config/termite
+  ln -sfn ${BASEDIR}/termite/config $HOME/.config/termite/config
 
-[ -d $HOME/.config/rofi/ ] || mkdir -p $HOME/.config/rofi
-ln -sfn ${BASEDIR}/rofi/config $HOME/.config/rofi/config
+  [ -d $HOME/.config/rofi/ ] || mkdir -p $HOME/.config/rofi
+  ln -sfn ${BASEDIR}/rofi/config $HOME/.config/rofi/config
 
-[ -d $HOME/.config/dunst/ ] || mkdir -p $HOME/.config/dunst
-ln -sfn ${BASEDIR}/dunst/dunstrc $HOME/.config/dunst/dunstrc
+  [ -d $HOME/.config/dunst/ ] || mkdir -p $HOME/.config/dunst
+  ln -sfn ${BASEDIR}/dunst/dunstrc $HOME/.config/dunst/dunstrc
 
-[ -d $HOME/.config/compton/ ] || mkdir -p $HOME/.config/compton
-ln -sfn ${BASEDIR}/compton/compton.conf $HOME/.config/compton/compton.conf
+  [ -d $HOME/.config/compton/ ] || mkdir -p $HOME/.config/compton
+  ln -sfn ${BASEDIR}/compton/compton.conf $HOME/.config/compton/compton.conf
 
-ln -sfn ${BASEDIR}/fontconfig $HOME/.config/fontconfig
+  ln -sfn ${BASEDIR}/fontconfig $HOME/.config/fontconfig
+fi
 
 echo "Installing Vim plugins..."
 vim -E -c PlugInstall -c qall
