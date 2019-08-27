a55b31e3ba30a5d4b19d51f4a8623c38f2e7c044
aonemd <aonemdsaleh@gmail.com>
2019-08-27 12:42:09 +0200

    Remove X11 config files

--- README.md
+++ README.md
@@ -1,20 +1,18 @@
 <div align="center">
   <h1>
-    aaku
+    aaku-server
   </h1>
 
   ![screenshot_1](https://user-images.githubusercontent.com/16504838/56446196-82acf180-6301-11e9-962b-fb7b3ead4dfd.png)
 </div>
 
-アーク (The Ark): My \*nix dotfiles.
-
-See more screenshots [here](https://imgur.com/a/uXBAy).
+アーク (The Ark): My \*nix dotfiles for the server.
 
 ### Installation
 
-1. Download the repo or use Git `git clone https://github.com/aonemd/aaku`
+1. Download the repo or use Git `git clone https://github.com/aonemd/aaku-server`
 2. Make a copy of `zsh/zsh/secrets.zsh.example` into `zsh/zsh/secrets.zsh` and add your own config and keys
-3. Run the installation script `cd aaku && ./install`
+3. Run the installation script `cd aaku-server && ./install`
 
 ### Notes
 
@@ -22,4 +20,4 @@ See more screenshots [here](https://imgur.com/a/uXBAy).
 
 ### License
 
-See [LICENSE](https://github.com/aonemd/aaku/blob/master/LICENSE).
+See [LICENSE](https://github.com/aonemd/aaku-server/blob/master/LICENSE).
--- install.sh
+++ install.sh
@@ -19,34 +19,6 @@ ln -sfn ${BASEDIR}/tmux/tmux.conf $HOME/.tmux.conf
 ln -sfn ${BASEDIR}/git/gitconfig $HOME/.gitconfig
 ln -sfn ${BASEDIR}/ag/agignore $HOME/.agignore
 
-if [[ $DISPLAY ]]; then
-  [ -d $HOME/.config/cmus/ ] || mkdir -p $HOME/.config/cmus
-  ln -sfn ${BASEDIR}/cmus/aaku.theme $HOME/.config/cmus/aaku.theme
-
-  ln -sfn ${BASEDIR}/X11/xinitrc $HOME/.xinitrc
-  ln -sfn ${BASEDIR}/X11/Xresources $HOME/.Xresources
-
-  ln -sfn ${BASEDIR}/i3 $HOME/.config/i3
-
-  ln -sfn ${BASEDIR}/i3blocks $HOME/.config/i3blocks
-
-  ln -sfn ${BASEDIR}/bin/fehbg $HOME/.fehbg
-
-  [ -d $HOME/.config/termite/ ] || mkdir -p $HOME/.config/termite
-  ln -sfn ${BASEDIR}/termite/config $HOME/.config/termite/config
-
-  [ -d $HOME/.config/rofi/ ] || mkdir -p $HOME/.config/rofi
-  ln -sfn ${BASEDIR}/rofi/config $HOME/.config/rofi/config
-
-  [ -d $HOME/.config/dunst/ ] || mkdir -p $HOME/.config/dunst
-  ln -sfn ${BASEDIR}/dunst/dunstrc $HOME/.config/dunst/dunstrc
-
-  [ -d $HOME/.config/compton/ ] || mkdir -p $HOME/.config/compton
-  ln -sfn ${BASEDIR}/compton/compton.conf $HOME/.config/compton/compton.conf
-
-  ln -sfn ${BASEDIR}/fontconfig $HOME/.config/fontconfig
-fi
-
 echo "Installing Vim plugins..."
 vim -E -c PlugInstall -c qall
 
