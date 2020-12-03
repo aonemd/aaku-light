221d9e61ba7405e8de1cc39b9695c9c711877efb
aonemd <aonemdsaleh@gmail.com>
2020-12-03 19:02:52 +0200

    Modify ts plugin to accept current location with `.`

---
+++ zsh/zsh/plugins/ts.zsh
@@ -0,0 +1,37 @@
+ts() {
+  [ -z "$1" ] && return
+
+  local session_path=""
+  case "$1" in
+    .)
+      session_path=$(pwd)
+      ;;
+    *)
+      session_path="${1}"
+  esac
+
+  local session_name="${session_path##*/}"
+
+  if tmux has-session -t="${session_name}" 2> /dev/null; then
+    tmux attach -t "${session_name}"
+    return
+  fi
+
+  pushd "${session_path}" > /dev/null
+
+  tmux new-session   -d -s "${session_name}"
+
+  tmux send-keys     -t="${session_name}":1 'vim ' C-m
+  sleep 0.5
+  tmux split-window  -t="${session_name}":1 -v
+  tmux resize-pane   -t="${session_name}":1 -D 15
+  tmux select-pane   -t="${session_name}":1.1
+
+  tmux new-window    -t="${session_name}":2 -n 'log'
+  tmux new-window    -t="${session_name}":3 -n 'misc'
+
+  tmux select-window -t="${session_name}":1
+  [ -n "$TMUX" ] && tmux switch -t="${session_name}" || tmux attach -t="${session_name}"
+
+  popd > /dev/null
+}
