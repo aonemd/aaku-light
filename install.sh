286e45511eff98e2faccca3b51f57472d88fef67
aonemd <aonemdsaleh@gmail.com>
2021-02-05 20:24:12 +0200

    Add ctags config file

--- install.sh
+++ install.sh
@@ -18,6 +18,7 @@ ln -sfn ${BASEDIR}/zsh/zshrc $HOME/.zshrc
 ln -sfn ${BASEDIR}/tmux/tmux.conf $HOME/.tmux.conf
 ln -sfn ${BASEDIR}/git/gitconfig $HOME/.gitconfig
 ln -sfn ${BASEDIR}/rg/rgignore $HOME/.rgignore
+ln -sfn ${BASEDIR}/ctags/ctags $HOME/.ctags
 
 echo "Installing Vim plugins..."
 vim -E -c PlugInstall -c qall
